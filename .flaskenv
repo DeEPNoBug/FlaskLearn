9dbe9559dc3d0155f7a769bca070d366fcce2af3
deepnobug <deepnobug@gmail.com>
2019-03-27 21:22:30 +0800

    python-dotenv 使用示例

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+# 用来存储和 Flask 相关的环境变量 例如: FLASK_APP
+FLASK_APP=app
--- app.py
+++ app.py
@@ -23,5 +23,15 @@ def greet(name):
     return "<h1> Hello, %s </h1>" % name
 
 
+"""
+使用 export 文件名 来指定运行的文件
+使用 flask run 命令可以在命令行启动开发服务器
+
+pipenv install python-dotenv
+》》 如果安装了 python-dotenv, flask run 会自动从 .flaskenv 和 .env
+手动设置的环境变量 > .env > .flaskenv
+在 .env 和 .flaskenv 中的环境变量都是用等号连接的 key=value 形式
+"""
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # 这种方式已经不被推荐
