5aba73f9b5596100b3cb83e75289ce855ed50dba
deepnobug <deepnobug@gmail.com>
2019-03-27 20:47:25 +0800

    最小Flask实例

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1> Hello, world! </h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
