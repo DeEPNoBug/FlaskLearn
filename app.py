742575fefb3ad48441b369d6458523ad6085085c
deepnobug <deepnobug@gmail.com>
2019-03-27 21:36:08 +0800

    改变默认监听 ip 和 端口

--- app.py
+++ app.py
@@ -31,6 +31,12 @@ pipenv install python-dotenv
 》》 如果安装了 python-dotenv, flask run 会自动从 .flaskenv 和 .env
 手动设置的环境变量 > .env > .flaskenv
 在 .env 和 .flaskenv 中的环境变量都是用等号连接的 key=value 形式
+
+命令行中使用 flask run --host=0.0.0.0 命令，使 flask 监听所有外部请求
+同样可以设置 FLASK_RUN_HOST 和 FLASK_RUN_PORT 来自定义可以访问的ip 和 对外端口
+可以使用 内网穿透/端口转发 工具 来让外部访问
+https://ngrok.com/
+https://localtunnel.github.io/www/
 """
 
 if __name__ == '__main__':
